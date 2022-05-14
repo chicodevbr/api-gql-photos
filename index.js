159b1d0673643d33edd9b84916a8c146414cd2cc
chicodevbr <chicodevbr@gmail.com>
2022-05-14 15:59:44 -0300

    :sparkles: feat: conectamdp ao db

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
 const { ApolloServer, gql } = require('apollo-server');
 const { importSchema } = require('graphql-import');
 const resolvers = require('./resolvers');
 
 const schemaPath = './schema/index.graphql';
+
+const dbUri = process.env.URI;
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(dbUri, dbOptions)
+  .then(() => console.log('\u{1F516} Database connected'))
+  .catch((error) => console.log('Databased failed: ', error));
 
 const server = new ApolloServer({
   typeDefs: importSchema(schemaPath),
